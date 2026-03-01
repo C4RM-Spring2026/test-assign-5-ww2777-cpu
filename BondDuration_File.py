ac17f4a8d520f9d2ef4303d8e1bbb78b6c1b2715
ww2777-cpu <ww2777@columbia.edu>
2026-03-01 17:14:32 -0500

    Update BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,8 +1,13 @@
 import numpy as np
 
-def getBondDuration(y, face, couponRate, m, ppy = 1):
-    if ppy == 1:
-        x = 8.51
-    if ppy == 2:
-        x = 8.42
-    return(x)
+
+def getBondDuration(y, face, couponRate, m, ppy=1):
+    periods=m*ppy
+    cf=face*couponRate/ppy
+    i=np.arange(1,periods+1)
+    discount=(1+y/ppy)**(-i)
+    pvsum=(cf*discount).sum()+face*(1+y/ppy)**(-periods)
+    wtsum=(i*cf*discount).sum()+periods*face*(1+y/ppy)**(-periods)
+    return((wtsum/pvsum)/ppy)
+
+
